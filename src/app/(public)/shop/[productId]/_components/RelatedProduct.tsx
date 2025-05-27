d7e43bcbc178bd713ccc29764374bb7d11ff66a7
dipujafar <dipujafarmr@gmail.com>
2025-05-27 17:41:44 +0600

    some change

--- src/app/(public)/shop/[productId]/_components/RelatedProduct.tsx
+++ src/app/(public)/shop/[productId]/_components/RelatedProduct.tsx
@@ -1,5 +1,6 @@
 import { ArrowIcon } from "@/components/icons/Icons";
 import Image from "next/image";
+import Link from "next/link";
 
 const charitySupportData = [
   {
@@ -30,7 +31,9 @@ const RelatedProduct = () => {
       {/* ======================================= section header ========================================== */}
       <div className="flex-between border-b lg:mb-6 mb-4 pb-2">
         <div className="flex items-center gap-x-1.5">
-          <h4 className="lg:text-3xl sm:text-xl text-lg font-medium uppercase">You may also like</h4>
+          <h4 className="lg:text-3xl sm:text-xl text-lg font-medium uppercase">
+            You may also like
+          </h4>
         </div>
         <div className="flex gap-x-3 items-center group">
           <h4 className=" font-bold">VIEW ALL</h4>
@@ -41,18 +44,20 @@ const RelatedProduct = () => {
       {/* ========================================= preview images and data ==================================== */}
       <div className="grid lg:grid-cols-4 grid-cols-2 md:gap-5 gap-2">
         {charitySupportData?.map((charitySupport) => (
-          <div key={charitySupport?._id} className="relative group">
-            <Image
-              src={charitySupport?.image}
-              alt="charity_support_data"
-              width={1200}
-              height={1200}
-              className="object-cover origin-center rounded-xl"
-            ></Image>
-            <div className="absolute flex-center bottom-0 w-full bg-[rgba(217,217,217,0.09)] text-white p-4 backdrop-blur-[7px] transition-all duration-1000 ease-in-out sm:h-[50px] h-[30px] group-hover:h-full group-hover:rounded-xl  md:group-hover:text-3xl group-hover:text-xl origin-bottom overflow-hidden">
-              <p>{charitySupport?.title}</p>
+          <Link href={"/shop/1"}  key={charitySupport?._id}>
+            <div className="relative group">
+              <Image
+                src={charitySupport?.image}
+                alt="charity_support_data"
+                width={1200}
+                height={1200}
+                className="object-cover origin-center rounded-xl"
+              ></Image>
+              <div className="absolute flex-center bottom-0 w-full bg-[rgba(217,217,217,0.09)] text-white p-4 backdrop-blur-[7px] transition-all duration-1000 ease-in-out sm:h-[50px] h-[30px] group-hover:h-full group-hover:rounded-xl  md:group-hover:text-3xl group-hover:text-xl origin-bottom overflow-hidden">
+                <p>{charitySupport?.title}</p>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
